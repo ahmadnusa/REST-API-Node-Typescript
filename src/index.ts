2a4da2a64169549a0c340f223b2900b1f3e6a435
ahmadnusa <ahmadnursaefullah79@gmail.com>
2023-12-15 10:16:35 +0700

    Setup ESlint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app: Application = express();
-const port: number = 3000;
+const app: Application = express()
+const port: number = 3000
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send("Hello World!");
-});
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/`);
-});
+  console.log(`Listening at http://localhost:${port}/`)
+})
