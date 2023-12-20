739315afd4bc6fa1933c13d25eaace9c2d00129b
ahmadnusa <ahmadnursaefullah79@gmail.com>
2023-12-20 15:57:29 +0700

    Remove requireUser middleware from productRouter

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -5,12 +5,12 @@ import {
   getProduct,
   updateProduct
 } from '../controllers/product.controller'
-import { requireAdmin, requireUser } from '../middleware/auth'
+import { requireAdmin } from '../middleware/auth'
 
 export const productRouter: Router = Router()
 
-productRouter.get('/', requireUser, getProduct)
-productRouter.get('/:id', requireUser, getProduct)
+productRouter.get('/', getProduct)
+productRouter.get('/:id', getProduct)
 productRouter.post('/', requireAdmin, createProduct)
 productRouter.put('/:id', requireAdmin, updateProduct)
 productRouter.delete('/:id', requireAdmin, deleteProduct)
